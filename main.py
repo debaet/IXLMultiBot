58501ec2e17babc30efa9272e1e1d4661a61e052
debaet <81795907+debaet@users.noreply.github.com>
2021-05-16 21:52:44 -0700

    removed bugs, will add them and fix in the future.

--- main.py
+++ main.py
@@ -6,24 +6,17 @@ import options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 import sys
-from faker import Faker 
+import colorama
 from colorama import Fore, Back, Style
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.by import By
 colorama.init()
 
-
 # config
-chrome_options = Options()
+
 # change the directory to your chrome driver. for more instructions read the readme file.os
 PATH = ('C:\\Program Files (x86)\\Chrome\\Application\\chromedriver.exe') 
-options = webdriver.ChromeOptions() 
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-options.add_argument("user-data-dir=C:\\Users\\gabri\\Local\\Google\\Chrome\\")
+
 
 driver = webdriver.Chrome(PATH)
-
 
 
 def main4(argv):
@@ -194,116 +187,22 @@ def op2():
 
 
 
-def op3():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print(Fore.MAGENTA + """\
-██╗██╗  ██╗██╗         ████████╗███████╗ █████╗  ██████╗██╗  ██╗███████╗██████╗     ██████╗  ██████╗ ████████╗
-██║╚██╗██╔╝██║         ╚══██╔══╝██╔════╝██╔══██╗██╔════╝██║  ██║██╔════╝██╔══██╗    ██╔══██╗██╔═══██╗╚══██╔══╝
-██║ ╚███╔╝ ██║            ██║   █████╗  ███████║██║     ███████║█████╗  ██████╔╝    ██████╔╝██║   ██║   ██║   
-██║ ██╔██╗ ██║            ██║   ██╔══╝  ██╔══██║██║     ██╔══██║██╔══╝  ██╔══██╗    ██╔══██╗██║   ██║   ██║   
-██║██╔╝ ██╗███████╗       ██║   ███████╗██║  ██║╚██████╗██║  ██║███████╗██║  ██║    ██████╔╝╚██████╔╝   ██║   
-╚═╝╚═╝  ╚═╝╚══════╝       ╚═╝   ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝    ╚═════╝  ╚═════╝    ╚═╝   """ + Style.RESET_ALL + Style.BRIGHT)
-
-    print(Fore.RED + "                            w3lcs: https://instagram.com/w3lcs"+ Style.RESET_ALL + Style.BRIGHT)
-
-    print(Fore.GREEN+ "[ OK ] ", end =""+ Style.RESET_ALL + Style.BRIGHT),
-    print("Loaded Modules")
-                                                                                                                
-    print(Fore.RED+ "[ NOTE ] ", end =""+ Style.RESET_ALL + Style.BRIGHT),
-    print("Code Will Loop Untill Bot Is Closed, Close Bot When You Get Your Desired Amount Of Accounts.")
-    print(Fore.RED+ "[ NOTE ] ", end =""+ Style.RESET_ALL + Style.BRIGHT),
-    print("Please Wait..")
-
-    f = Faker()
-
-    driver.get('https://www.ixl.com/membership/teachers/trial')
-
-
-    # Your Information
-
-    while True:
-        driver.find_element_by_xpath('/html/body/main/form/section[2]/div[1]/input').send_keys(f.name())
-        driver.execute_script("window.open('');")
-        driver.switch_to.window(driver.window_handles[1])
-        time.sleep(1)
-        driver.get("https://snapchat.tf/")
-        driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[1]').click()
-        driver.switch_to.window(driver.window_handles[0])
-        time.sleep(2)
-        driver.find_element_by_xpath('/html/body/main/form/section[2]/div[2]/input').send_keys(Keys.CONTROL, 'v')
-
-
-        # Your School
-        driver.find_element_by_xpath('/html/body/main/form/section[3]/div/div[2]/div[1]/input').send_keys('90011')
-        time.sleep(2)
-        driver.find_element_by_class_name('select-institutionId').click()
-        driver.find_element_by_xpath('/html/body/main/form/section[3]/div/div[2]/div[2]/div/select/optgroup[1]/option[1]').click()
-
-
-        # IXL Quantity Selection
-        driver.find_element_by_xpath('/html/body/main/form/section[4]/div[1]/input').send_keys(f.word())
-        driver.find_element_by_xpath('/html/body/main/form/section[4]/div[1]/input').send_keys(f.word())
-        driver.find_element_by_xpath('/html/body/main/form/section[4]/div[1]/input').send_keys(f.word())
-        driver.find_element_by_xpath('/html/body/main/form/section[4]/div[2]/div[2]/label').click()
-        driver.find_element_by_xpath('/html/body/main/form/section[4]/div[2]/div[1]/label').click()
-        driver.find_element_by_xpath('/html/body/main/form/section[4]/div[2]/div[3]/label').click()
-        driver.find_element_by_xpath('/html/body/main/form/section[4]/div[2]/div[4]/label').click()
-        driver.find_element_by_xpath('/html/body/main/form/section[4]/div[6]/div/button').click()
-        time.sleep(2)
-
-        # Printing Information For User
-        driver.switch_to.window(driver.window_handles[1])
-        driver.refresh()
-        driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[2]').click()
-        time.sleep(1)
-        driver.refresh()
-        time.sleep(2)
-        driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[2]').click()
-        time.sleep(1)
-        driver.refresh()
-        time.sleep(2)
-        driver.refresh()
-        driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[2]').click()
-        time.sleep(1)
-        driver.refresh()
-        time.sleep(2)
-        driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[2]/div[2]/div[2]').click()
-        time.sleep(1)
-        driver.refresh()
-        time.sleep(5)
-        driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/main/div[1]/div/div').click()
-        time.sleep(2)
-
-
-        username = driver.find_element_by_xpath('/html/body/main/div[1]/div/div[3]/div/div/div[5]/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td')
-
-        username2 = username.text
-        print(Fore.RED+ "[ SUCCESS ] ", end =""+ Style.RESET_ALL + Style.BRIGHT),
-        print(username2)
-        password = driver.find_element_by_xpath('/html/body/main/div[1]/div/div[3]/div/div/div[5]/table/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[3]/td')
-        password2 = password.text
-        print(Fore.RED+ "[ SUCCESS ] ", end =""+ Style.RESET_ALL + Style.BRIGHT),
-        print(password2)
-        driver.delete_all_cookies()
-        driver.close();
-        driver.switch_to.window(driver.window_handles[0])
-        driver.get('https://www.ixl.com/membership/teachers/trial')
-
-
 
 def op4():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print('This is not done yet. Please watch for updates either on reddit or github.')
+    print('If any of these are incorrect, bot will fail.')
+    username = input('Enter Username or Email: ')
+    password = input('Enter Password:  ')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
     
 
 def op5():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print('Coded by @w3lcs')
-    print('MY OTHER SOCIALS ARE')
-    print('@debaet , REDDIT U/Q3LCS')
-    print('MY GITHUB IS @DEBAET')
-    
+    print('If any of these are incorrect, bot will fail.')
+    username = input('Enter Username or Email: ')
+    password = input('Enter Password:  ')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
     
 
